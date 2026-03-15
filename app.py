5e4da2b6099a1fab1586a5b5ddbcb5a520cf0ed3
Lucky <lucky@Luckys-MacBook-Air.local>
2026-03-15 14:49:18 -0300

    Update in app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 app = Flask(__name__)
 
 REQUEST_COUNT = Counter(
-    "helloops_request_total",
-    "Total HTTPS request from the helloops"
+    "helloops_requests_total",
+    "Total HTTP requests to helloops"
 )
 
 @app.before_request
@@ -18,6 +18,7 @@ def before_request():
 @app.route("/")
 def home():
     api_key_present = bool(os.getenv("APP_API_KEY"))
+
     return jsonify({
         "app": "helloops",
         "version": os.getenv("APP_VERSION", "v1"),
@@ -29,13 +30,12 @@ def home():
 @app.route("/health")
 def health():
     return jsonify({
-        "status" : "OK"
+        "status": "ok"
     })
 
 @app.route("/metrics")
 def metrics():
-    return generate_latest(), 200, {"content_type" : CONTENT_TYPE_LATEST}
+    return generate_latest(), 200, {"Content-Type": CONTENT_TYPE_LATEST}
 
-
-if __name__== "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
